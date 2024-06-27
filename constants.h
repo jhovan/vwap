d49c50b7abded09ba7862a24a4a707995cab5007
Jhovan <jhovan.gallardo@gmail.com>
2024-06-26 22:00:24 -0700

    Add constants.h

---
+++ constants.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <unordered_map>
+
+using namespace std;
+
+inline const unordered_map<char,int> SIZE_MAP = {
+    {'S', 12},
+    {'R', 39},
+    {'H', 25},
+    {'Y', 20},
+    {'L', 26},
+    {'V', 35},
+    {'W', 12},
+    {'K', 28},
+    {'J', 35},
+    {'h', 21},
+    {'A', 36},
+    {'F', 40},
+    {'E', 31},
+    {'C', 36},
+    {'X', 23},
+    {'D', 19},
+    {'U', 35},
+    {'P', 44},
+    {'Q', 40},
+    {'B', 19},
+    {'I', 50},
+    {'N', 20},
+    {'O', 48},
+};
--- vwap.cpp
+++ vwap.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "constants.h"
 
 using namespace std;
 
