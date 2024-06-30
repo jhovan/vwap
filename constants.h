a9ea6aad1f2efd1670190718c9dcb25940933853
Jhovan <jhovan.gallardo@gmail.com>
2024-06-29 21:55:50 -0700

    Add MessageAttribiute enum

--- constants.h
+++ constants.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include<unordered_map>
+
 // Sizes in Bytes
 
 inline const int BUFFER_SIZE = 64000;
 
 // It corresponds to Trade message type (including 2 extra bytes to store size itself)
 inline const int MAX_MESSAGE_SIZE = 52;
+
+enum class MessageAttribute {
+    Timestamp,
+    RefNo,
+    MatchNo,
+    Shares,
+    Stock,
+    Price,
+    Printable,
+};
