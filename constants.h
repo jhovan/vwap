ed320a23fc5173a9da89e13706fe6a2a3c6e5893
Jhovan <jhovan.gallardo@gmail.com>
2024-06-30 05:10:15 -0700

    Add extra NANOSECONDS_IN_HOUR

--- constants.h
+++ constants.h
@@ -10,6 +10,9 @@ inline const int BUFFER_SIZE = 64000;
 // It corresponds to Trade message type (including 2 extra bytes to store size itself)
 inline const int MAX_MESSAGE_SIZE = 52;
 
+// Nanoseconds in 1 hour
+inline const u_int64_t  NANOSECONDS_IN_HOUR = 3600000000000;
+
 // Relevant message attributes (similar names and data types grouped)
 enum class MessageAttribute:char {
     Timestamp,
@@ -17,21 +20,31 @@ enum class MessageAttribute:char {
     NewRefNo,
     MatchNo,
     Shares,
+    LongShares,
+    StockLocale,
     Stock,
     Price,
     Printable,
 };
 
 // Relevant message attributes offsets and sizes by type
-inline const std::unordered_map<char,std::unordered_map<MessageAttribute, std::pair<uint8_t, uint8_t>>> SIZE_MAP = {
+inline const std::unordered_map<char,std::unordered_map<MessageAttribute, std::pair<uint8_t, uint8_t>>> GLOBAL_ATTRIBUTE_MAP = {
     {
-        // Add Order – No MPID Attribution
+        // Stock Directory
+        'R', 
+        {
+            {MessageAttribute::StockLocale, {1, 2}},
+            {MessageAttribute::Timestamp, {5, 6}},
+            {MessageAttribute::Stock, {11, 8}}
+        },
+    },
+    {
         'A', 
         {
+            {MessageAttribute::StockLocale, {1, 2}},
             {MessageAttribute::Timestamp, {5, 6}},
             {MessageAttribute::RefNo, {11, 8}},
             {MessageAttribute::Shares, {20, 4}},
-            {MessageAttribute::Stock, {24, 8}},
             {MessageAttribute::Price, {32, 4}}
         }
     },
@@ -39,10 +52,10 @@ inline const std::unordered_map<char,std::unordered_map<MessageAttribute, std::p
         // Add Order with MPID Attribution
         'F', 
         {
+            {MessageAttribute::StockLocale, {1, 2}},
             {MessageAttribute::Timestamp, {5, 6}},
             {MessageAttribute::RefNo, {11, 8}},
             {MessageAttribute::Shares, {20, 4}},
-            {MessageAttribute::Stock, {24, 8}},
             {MessageAttribute::Price, {32, 4}}
         }
     },
@@ -100,20 +113,20 @@ inline const std::unordered_map<char,std::unordered_map<MessageAttribute, std::p
         // Trade Message (Non-Cross)
         'P', 
         {
+            {MessageAttribute::StockLocale, {1, 2}},
             {MessageAttribute::Timestamp, {5, 6}},
             {MessageAttribute::Shares, {20, 4}},
-            {MessageAttribute::Stock, {24, 8}},
             {MessageAttribute::Price, {32, 4}},
             {MessageAttribute::MatchNo, {36, 8}}
         }
     },
     {
-        // Cross Trade Message (Not used, since it gets sent in bulk print per symbol)
+        // Cross Trade Message 
         'Q', 
         {
+            {MessageAttribute::StockLocale, {1, 2}},
             {MessageAttribute::Timestamp, {5, 6}},
-            {MessageAttribute::Shares, {11, 8}},
-            {MessageAttribute::Stock, {19, 8}},
+            {MessageAttribute::LongShares, {11, 8}},
             {MessageAttribute::Price, {27, 4}},
             {MessageAttribute::MatchNo, {31, 8}}
         }
