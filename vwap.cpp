ce2ae4576a8422b6d9adf841b0aee26837369ff3
Jhovan <jhovan.gallardo@gmail.com>
2024-07-02 07:54:51 -0700

    Add command line argument support

--- vwap.cpp
+++ vwap.cpp
@@ -5,13 +5,30 @@
 using namespace std;
 using namespace std::chrono;
 
-int main() 
+int main(int argc, char* argv[]) 
 {
-    string file_name = "input";
+    string file_name;
+    // Assigns input name, by argument or default value
+    if (argc > 1)
+    {
+        file_name = argv[1];
+    }
+    else
+    {
+        file_name = "input";
+    }
     BinaryIngester bi{file_name};
     auto start = high_resolution_clock::now();
     bi.processBinary();
-    bi.save_vwap_to_cvs();
+    // Assigns output name, by argument or default value
+    if (argc > 2)
+    {
+        bi.save_vwap_to_cvs(argv[2]);
+    }
+    else
+    {
+        bi.save_vwap_to_cvs();
+    }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<seconds>(stop - start);
     cout << "Time: " << duration.count() << " seconds" << endl;
